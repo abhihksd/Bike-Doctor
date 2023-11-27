a7b3a1b8f01f731fb445cb06720970b89545ddb0
Abhishek Shujalpurkar <46176211+abhihksd@users.noreply.github.com>
2023-11-27 18:06:52 +0530

    bike insert data sql file added

---
+++ customer_insert_data.sql
@@ -0,0 +1,13 @@
+INSERT INTO `customer` (`cust_name`, `mobile`, `email`, `bike_id`) VALUES
+('Amit Patel', '9876543210', 'amit.patel@email.com', 1),
+('Priya Sharma', '8765432109', 'priya.sharma@email.com', 2),
+('Rahul Verma', '8989898989', 'rahul.verma@email.com', 3),
+('Ananya Singh', '8765432108', 'ananya.singh@email.com', 4),
+('Sandeep Kumar', '9876543211', 'sandeep.kumar@email.com', 5),
+('Neha Gupta', '8989898988', 'neha.gupta@email.com', 6),
+('Rajat Bansal', '8765432107', 'rajat.bansal@email.com', 7),
+('Kavita Patel', '9876543212', 'kavita.patel@email.com', 8),
+('Rohit Tiwari', '8989898987', 'rohit.tiwari@email.com', 9),
+('Sarika Singh', '8765432106', 'sarika.singh@email.com', 10);
+
+select * from customer;
---
+++ service_center_insert.sql
@@ -0,0 +1,13 @@
+INSERT INTO `service_center` (`sc_name`, `address`, `mobile`, `no_of_mechanic`) VALUES
+('Rajput Bikes', '1 Rajput Road, Jaipur', '9876543210', '3'),
+('Maratha Motors', '2 Maratha Street, Pune', '8765432109', '4'),
+('Ganga Cycles', '3 Ganga Lane, Varanasi', '8989898989', '5'),
+('Chennai Speed', '4 Chennai Avenue, Chennai', '8765432108', '2'),
+('Bengaluru Bikes', '5 Bengaluru Street, Bangalore', '9876543211', '3'),
+('Mumbai Cycles', '6 Mumbai Road, Mumbai', '8989898988', '4'),
+('Delhi Motors', '7 Delhi Lane, Delhi', '8765432107', '5'),
+('Kolkata Wheels', '8 Kolkata Avenue, Kolkata', '9876543212', '2'),
+('Hyderabad Bikes', '9 Hyderabad Street, Hyderabad', '8989898987', '3'),
+('Ahmedabad Motors', '10 Ahmedabad Lane, Ahmedabad', '8765432106', '4');
+
+select * from service_center;
